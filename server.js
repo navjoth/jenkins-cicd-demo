1d1a9f2f7003beb15920181a134880bd88a94abb
Navjoth <navjothrai@gmail.com>
2025-03-28 12:58:04 +0530

    changed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello from Jenkins CI/CD!'));
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
